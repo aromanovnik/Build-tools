831a04fa52ed67383fbc47ff7347d99197264b5b
alekseyromanov <aromanovnik@gmail.com>
2021-09-23 21:59:43 +0300

    asd

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import {diffDates, diffToHtml} from "./datecalc.js"; // 1
 import {formatError} from "./utils.js"; // 2
 
 const dateCalcForm = document.getElementById("datecalc");
+const timerForm = document.getElementById("timer");
 const dateCalcResult = document.getElementById("datecalc__result");
 
 dateCalcForm.addEventListener("submit", handleCalcDates);
+timerForm.addEventListener("submit", handlerTimer);
 
 function handleCalcDates(event) {
     dateCalcResult.innerHTML = "";
@@ -18,4 +20,10 @@ function handleCalcDates(event) {
         const diff = diffDates(firstDateValue, secondDateValue); // 3
         dateCalcResult.innerHTML = diffToHtml(diff); // 4
     } else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); // 5
+}
+
+function handlerTimer(event) {
+    event.preventDefault();
+
+    console.dir(event.target)
 }
