df60b98335c07ce365220a2b5623e77d2f9e6365
Aleksey Romanov <badkidddd@gmail.com>
2021-09-27 16:05:05 +0300

    howler

--- src/timer.js
+++ src/timer.js
@@ -1,8 +1,4 @@
-// export function handlerTimer(event) {
-//     event.preventDefault();
-//
-//     console.dir(event.target)
-// }
+import 'https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js';
 
 export const timer = {
     settings: {
@@ -10,41 +6,67 @@ export const timer = {
         displayBlockId: 'timerDisplay',
         interval: 1000,
     },
-    time: 0,
+    // time: 0,
     intervalId: undefined,
 
+    // The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page.
+    // sound: new Howl({
+    //     src: ['./beep.mp3'],
+    //     html5: true,
+    //     autoplay: false,
+    //     volume: 0.8,
+    // }),
+
+    _time: 0,
+    set time(ms) {
+        this._time = ms;
+    },
+
+    get time() {
+        return this._time;
+    },
+
     start() {
-        console.log(this.settings)
+        this.playSound();
         const input = document.getElementById(this.settings.inputId)?.value;
         if (!input) {
             return;
         }
 
         this.time = +input * 60 * 1000; // In ms
-        console.log(this.time)
         if (!this.time) {
             return;
         }
 
-        console.log('Start');
+        this.displayTime();
+
         this.intervalId = setInterval(() => {
             this.time -= this.settings.interval;
             this.displayTime();
 
             if (this.time <= 0) {
                 this.stop();
+                this.playSound();
             }
         }, this.settings.interval);
     },
 
     stop() {
-        console.log('Stop');
         if (this.intervalId) {
             clearInterval(this.intervalId);
         }
 
         this.time = 0;
         this.displayTime();
+    },
+
+    playSound() {
+        (new Howl({
+            src: ['./src/beep.mp3'],
+            html5: true,
+            autoplay: false,
+            volume: 0.8,
+        }))?.play();
     },
 
     msToMinutesAndSeconds(ms) {
@@ -54,7 +76,6 @@ export const timer = {
     },
 
     displayTime() {
-        console.log(this.time);
         const displayBlockId = document.getElementById(this.settings.displayBlockId);
         if (!displayBlockId) {
             return;
